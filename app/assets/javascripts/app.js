0f828847350ff0a1e9cc18a78f4aacdbc890e118
Joanna Johnson <jjohnson@iqt.org>
2014-09-12 19:06:07 -0400

    Respond to search button. Conduct search.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,4 @@
-var controllers, receta;
+var controllers, receta, recipes;
 
 receta = angular.module('receta', ['templates', 'ngRoute', 'controllers']);
 
@@ -11,6 +11,37 @@ receta.config([
   }
 ]);
 
+recipes = [
+  {
+    id: 1,
+    name: 'Baked Potato w/ Cheese'
+  }, {
+    id: 2,
+    name: 'Garlic Mashed Potatoes'
+  }, {
+    id: 3,
+    name: 'Potatoes Au Gratin'
+  }, {
+    id: 4,
+    name: 'Baked Brussel Sprouts'
+  }
+];
+
 controllers = angular.module('controllers', []);
 
-controllers.controller("RecipesController", ['$scope', function($scope) {}]);
+controllers.controller("RecipesController", [
+  '$scope', '$routeParams', '$location', function($scope, $routeParams, $location) {
+    var keywords;
+    $scope.search = function(keywords) {
+      return $location.path("/").search('keywords', keywords);
+    };
+    if ($routeParams.keywords) {
+      keywords = $routeParams.keywords.toLowerCase();
+      return $scope.recipes = recipes.filter(function(recipe) {
+        return recipe.name.toLowerCase().indexOf(keywords) !== -1;
+      });
+    } else {
+      return $scope.recipes = [];
+    }
+  }
+]);
