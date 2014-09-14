a71e47472879200c493c7883e2b7a22c4f325a3a
Joanna Johnson <jjohnson@iqt.org>
2014-09-13 23:17:00 -0400

    Add recipe show route, controller, view and passing specs.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,8 @@ class RecipesController < ApplicationController
   def index
     @recipes = params[:keywords] ? Recipe.where('name ilike ?',"%#{params[:keywords]}%") : []
   end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Receta::Application.routes.draw do
-  resources :recipes, only: [:index]
+  resources :recipes, only: [:index, :show]
   root 'home#index'
 end
