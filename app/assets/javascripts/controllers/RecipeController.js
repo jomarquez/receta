c5c32ff46b4ca9a7c4181a75877d140f4667728b
Joanna Johnson <jjohnson@iqt.org>
2014-09-14 02:14:13 -0400

    Add Create, Update, Delete to our front end controller. Add teaspoon specs to test this new functionality.

--- app/assets/javascripts/controllers/RecipeController.js
+++ app/assets/javascripts/controllers/RecipeController.js
@@ -8,6 +8,13 @@ controllers.controller("RecipeController", [
     Recipe = $resource('/recipes/:recipeId', {
       recipeId: "@id",
       format: 'json'
+    }, {
+      'save': {
+        method: 'PUT'
+      },
+      'create': {
+        method: 'POST'
+      }
     });
     if ($routeParams.recipeId) {
       Recipe.get({
@@ -23,6 +30,35 @@ controllers.controller("RecipeController", [
     }
     $scope.back = function() {
       return $location.path("/");
+    };
+    $scope.edit = function() {
+      return $location.path("/recipes/" + $scope.recipe.id + "/edit");
+    };
+    $scope.cancel = function() {
+      if ($scope.recipe.id) {
+        return $location.path("/recipes/" + $scope.recipe.id);
+      } else {
+        return $location.path("/");
+      }
+    };
+    $scope.save = function() {
+      var onError;
+      onError = function(_httpResponse) {
+        return flash.error = "Something went wrong";
+      };
+      if ($scope.recipe.id) {
+        return $scope.recipe.$save((function() {
+          return $location.path("/recipes/" + $scope.recipe.id);
+        }), onError);
+      } else {
+        return Recipe.create($scope.recipe, (function(newRecipe) {
+          return $location.path("/recipes/" + newRecipe.id);
+        }), onError);
+      }
+    };
+    return $scope["delete"] = function() {
+      $scope.recipe.$delete();
+      return $scope.back();
     };
   }
 ]);
--- spec/javascripts/controllers/RecipeController_spec.js
+++ spec/javascripts/controllers/RecipeController_spec.js
@@ -1,31 +1,39 @@
 describe("RecipeController", function() {
-  var ctrl, fakeRecipe, httpBackend, flash, recipeId, routeParams, scope, setupController;
+  var ctrl, fakeRecipe, flash, httpBackend, location, recipeId, routeParams, scope, setupController;
   scope = null;
   ctrl = null;
   routeParams = null;
   httpBackend = null;
   flash = null;
+  location = null;
   recipeId = 42;
   fakeRecipe = {
     id: recipeId,
     name: "Baked Potatoes",
     instructions: "Pierce potato with fork, nuke for 20 minutes"
   };
-  setupController = function(recipeExists) {
+  setupController = function(recipeExists, recipeId) {
     if (recipeExists == null) {
       recipeExists = true;
     }
+    if (recipeId == null) {
+      recipeId = 42;
+    }
     return inject(function($location, $routeParams, $rootScope, $httpBackend, $controller, _flash_) {
-      var location, request, results;
+      var request, results;
       scope = $rootScope.$new();
       location = $location;
       httpBackend = $httpBackend;
       routeParams = $routeParams;
-      routeParams.recipeId = recipeId;
-      flash = _flash_
-      request = new RegExp("\/recipes/" + recipeId);
-      results = recipeExists ? [200, fakeRecipe] : [404];
-      httpBackend.expectGET(request).respond(results[0], results[1]);
+      if (recipeId) {
+        routeParams.recipeId = recipeId;
+      }
+      flash = _flash_;
+      if (recipeId) {
+        request = new RegExp("\/recipes/" + recipeId);
+        results = recipeExists ? [200, fakeRecipe] : [404];
+        httpBackend.expectGET(request).respond(results[0], results[1]);
+      }
       return ctrl = $controller('RecipeController', {
         $scope: scope
       });
@@ -36,7 +44,7 @@ describe("RecipeController", function() {
     httpBackend.verifyNoOutstandingExpectation();
     return httpBackend.verifyNoOutstandingRequest();
   });
-  return describe('controller initialization', function() {
+  describe('controller initialization', function() {
     describe('recipe is found', function() {
       beforeEach(setupController());
       return it('loads the given recipe', function() {
@@ -53,5 +61,60 @@ describe("RecipeController", function() {
       });
     });
   });
+  describe('create', function() {
+    var newRecipe;
+    newRecipe = {
+      id: 42,
+      name: 'Toast',
+      instructions: 'put in toaster, push lever, add butter'
+    };
+    beforeEach(function() {
+      var request;
+      setupController(false, false);
+      request = new RegExp("\/recipes");
+      return httpBackend.expectPOST(request).respond(201, newRecipe);
+    });
+    return it('posts to the backend', function() {
+      scope.recipe.name = newRecipe.name;
+      scope.recipe.instructions = newRecipe.instructions;
+      scope.save();
+      httpBackend.flush();
+      return expect(location.path()).toBe("/recipes/" + newRecipe.id);
+    });
+  });
+  describe('update', function() {
+    var updatedRecipe;
+    updatedRecipe = {
+      name: 'Toast',
+      instructions: 'put in toaster, push lever, add butter'
+    };
+    beforeEach(function() {
+      var request;
+      setupController();
+      httpBackend.flush();
+      request = new RegExp("\/recipes");
+      return httpBackend.expectPUT(request).respond(204);
+    });
+    return it('posts to the backend', function() {
+      scope.recipe.name = updatedRecipe.name;
+      scope.recipe.instructions = updatedRecipe.instructions;
+      scope.save();
+      httpBackend.flush();
+      return expect(location.path()).toBe("/recipes/" + scope.recipe.id);
+    });
+  });
+  return describe('delete', function() {
+    beforeEach(function() {
+      var request;
+      setupController();
+      httpBackend.flush();
+      request = new RegExp("\/recipes/" + scope.recipe.id);
+      return httpBackend.expectDELETE(request).respond(204);
+    });
+    return it('posts to the backend', function() {
+      scope["delete"]();
+      httpBackend.flush();
+      return expect(location.path()).toBe("/");
+    });
+  });
 });
-RunLink
